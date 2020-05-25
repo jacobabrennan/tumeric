575b534098c4b631d7b10d5b025eb04ce7da0ce1
Jacob A Brennan <jacobabrennan@gmail.com>
2020-05-24 22:23:53 -0400

    Create canvas stub

--- client/view/gameplay.js
+++ client/view/gameplay.js
@@ -5,7 +5,20 @@
 //-- Dependencies --------------------------------
 import view, { ID_VIEW_GAMEPLAY } from './index.js';
 
+//-- Constants -----------------------------------
+const TAG_CANVAS = 'canvas';
+const CANVAS_WIDTH = 256;
+const CANVAS_HEIGHT = Math.floor(CANVAS_WIDTH * (9/16));
+
 //------------------------------------------------
 export default Object.assign(Object.create(view), {
     elementId: ID_VIEW_GAMEPLAY,
+    configure(configuration) {
+        const container = document.getElementById(this.elementId);
+        const canvas = document.createElement(TAG_CANVAS);
+        canvas.width = CANVAS_WIDTH;
+        canvas.height = CANVAS_HEIGHT;
+        canvas.style.background = 'blue'
+        container.appendChild(canvas);
+    }
 });
